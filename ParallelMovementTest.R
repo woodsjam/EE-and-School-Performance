619909167a706370849f949758c1ee7fc92eab51
James Woods <woodsj@psu.ds.pdx.edu>
2018-01-18 13:45:31 -0800

    Final slides for WWU

---
+++ ParallelMovementTest.R
@@ -0,0 +1,24 @@
+library(car)
+
+EMath
+
+linearHypothesis(EMath,c("PlaceboTRUE = 0","EffectiveTRUE = 0" ) )
+linearHypothesis(ESci,c("PlaceboTRUE = 0","EffectiveTRUE = 0" ) )
+linearHypothesis(EELA,c("PlaceboTRUE = 0","EffectiveTRUE = 0" ) )
+linearHypothesis(ERead,c("PlaceboTRUE = 0","EffectiveTRUE = 0" ) )
+linearHypothesis(EWrite,c("PlaceboTRUE = 0","EffectiveTRUE = 0" ) )
+linearHypothesis(EAttend,c("PlaceboTRUE = 0","EffectiveTRUE = 0" ) )
+
+linearHypothesis(MMath,c("PlaceboTRUE = 0","EffectiveTRUE = 0" ) )
+linearHypothesis(MSci,c("PlaceboTRUE = 0","EffectiveTRUE = 0" ) )
+linearHypothesis(MELA,c("PlaceboTRUE = 0","EffectiveTRUE = 0" ) )
+linearHypothesis(MRead,c("PlaceboTRUE = 0","EffectiveTRUE = 0" ) )
+linearHypothesis(MWrite,c("PlaceboTRUE = 0","EffectiveTRUE = 0" ) )
+linearHypothesis(MAttend,c("PlaceboTRUE = 0","EffectiveTRUE = 0" ) )
+
+linearHypothesis(HMath,c("PlaceboTRUE = 0","EffectiveTRUE = 0" ) )
+linearHypothesis(HSci,c("PlaceboTRUE = 0","EffectiveTRUE = 0" ) )
+linearHypothesis(HELA,c("PlaceboTRUE = 0","EffectiveTRUE = 0" ) )
+linearHypothesis(HRead,c("PlaceboTRUE = 0","EffectiveTRUE = 0" ) )
+linearHypothesis(HWrite,c("PlaceboTRUE = 0","EffectiveTRUE = 0" ) )
+linearHypothesis(HAttend,c("PlaceboTRUE = 0","EffectiveTRUE = 0" ) )
